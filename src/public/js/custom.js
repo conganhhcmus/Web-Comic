42480449417c720331dd59e30a3655efbc29772a
TranNhutKhaKHTN <trannhutkha123456>
2020-01-09 18:11:48 +0700

    update next/prev chapter

--- src/public/js/custom.js
+++ src/public/js/custom.js
@@ -27,3 +27,22 @@ function submitPrevForm() {
         $('form').submit();
     }
 }
+
+var scrollableElement = document.body; //document.getElementById('scrollableElement');
+scrollableElement.addEventListener('wheel', checkScrollDirection);
+var a = document.getElementById("chap");
+function checkScrollDirection(event) {
+  if (checkScrollDirectionIsUp(event)) {
+    a.style.display = "block";
+    a.style.position="fixed";
+  } 
+  else {
+    a.style.display = "none";
+  }
+}
+function checkScrollDirectionIsUp(event) {
+  if (event.wheelDelta) {
+    return event.wheelDelta > 0;
+  }
+  return event.deltaY < 0;
+}
